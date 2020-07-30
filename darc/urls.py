7d4952bc554ebcd64472092bb2666bec5a92560d
Alex Zhang <alex@alz.dev>
2020-07-30 15:51:44 -0400

    Add Django built-in user authentication
    Uses django.contrib.auth.views.LoginView and LogoutView to facilitate
    user authentication. The login template uses Bootstrap 4.5 provided by
    Cloudflare cdnjs.

--- darc/urls.py
+++ darc/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.urls import include, path
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('login/', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
